41e8f38a7eccf0c5858fd70b5ed96f0af7162641
Ashley Schoonover <aschoo@umich.edu>
2018-01-31 13:08:51 -0500

    updated changes in branch add questions

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,12 @@
+import random
 def question:
-	response = input("what is your question?")
-	return response
+	ask = input("what is your question?")
+	return ask
+possible_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, Definitely", "You may rely on it",
+"As I see it, Yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply Hazey, try again",
+"Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't Count on it",
+"My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+question()
+
+print(random.choice(possible_answers))
