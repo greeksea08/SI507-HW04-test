0fa5519fca0a7d197c70a54c2c5086375c6f2632
Aegean Buenafe <aegean@Aegeans-MacBook-Pro.local>
2018-01-31 13:16:50 -0500

    added checking questions code

--- magic_eight.py
+++ magic_eight.py
@@ -1,16 +1,29 @@
 import random
 
 def question():
-	ask = input("what is your question?")
-	return ask 
+	ask = input("What is your question?")
+	return ask
 
-possible_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, Definitely", "You may rely on it",
-"As I see it, Yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply Hazey, try again",
-"Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't Count on it",
+possible_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it",
+"As I see it, Yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again",
+"Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it",
 "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
-question()
+ask_prompt = ""
+while ask_prompt != "quit":
+	ask_prompt = question()
+	if ask_prompt.endswith("?"):
+		print(random.choice(possible_answers))
+	elif ask_prompt == "quit":
+		print("END")
+	else:
+		print("I’m sorry, I can only answer questions(hint:?).\nTry again or type 'quit' to exit.")
 
-print(random.choice(possible_answers))
+
+
+
+
+	
+
 
 
